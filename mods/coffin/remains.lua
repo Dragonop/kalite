71164a249c44149cce12ac0bc77064ba2d0dd005
James Stevenson <everamzah@gmail.com>
2016-01-19 11:59:00 -0500

    Proper setting of remains

--- mods/coffin/remains.lua
+++ mods/coffin/remains.lua
@@ -3,27 +3,17 @@ minetest.register_on_dieplayer(function(player)
 	local param2 = minetest.dir_to_facedir(player:get_look_dir())
 	
 	local nn = minetest.get_node(pos).name
-	print("Node name is: " .. nn)
 
 	local name = player:get_player_name()
 	local protected = false
 	if minetest.is_protected(pos, name)
 			or nn == "default:cloud"
 			or string.match(nn, "protector:protect") then
-		-- Raise remains a node so we don't override other
-		-- player's protected areas.
 		pos.y = pos.y + 1
 		protected = true
 		print(nn .. " is protected")
 	end
---[[
-	for i = 1, player_inv:get_size("main") do
-		player_inv:set_stack("main", i, nil)
-	end
-	for i = 1, player_inv:get_size("craft") do
-		player_inv:set_stack("craft", i, nil)
-	end
---]]
+
 	local meta
 	local player_inv = player:get_inventory()
 	if not protected then
@@ -36,6 +26,7 @@ minetest.register_on_dieplayer(function(player)
 		meta:set_string("infotext", "RIP " .. name)
 
 		pos.y = pos.y - 1
+		meta = minetest.get_meta(pos)
 		local inv = meta:get_inventory()
 		inv:set_size("main", 8 * 4)
 		
