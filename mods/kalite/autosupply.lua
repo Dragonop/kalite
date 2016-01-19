111f5b19bcf5fbbf65d2127979edb877299ade7d
James Stevenson <everamzah@gmail.com>
2016-01-19 00:49:04 -0500

    Modify autosupplies Try to make an effecient use of ABMs Remove igniters

--- mods/kalite/autosupply.lua
+++ mods/kalite/autosupply.lua
@@ -1,300 +1,414 @@
 -- AUTO GENERATION
 
--- Tree
+local function remove_igniters(pos)
+	local igniters = minetest.find_node_near(pos, 9, "group:igniter")
+	if igniters then
+		minetest.remove_node(igniters)
+		return remove_igniters(pos)
+	end
+end
 
+-- Tree
 minetest.register_node("kalite:dirt", {
-	description="Dirt (Tree)",
-	tiles={"default_dry_dirt.png"},
-	is_ground_content=true,
-	groups={
-		crumbly=default.dig.dirt, soil=1
+	description = "Dirt (Tree)",
+	tiles = {"default_dry_dirt.png"},
+	is_ground_content = true,
+	groups = {
+		crumbly = default.dig.dirt,
+		soil = 1,
+		not_in_creative_inventory = 1
 	},
-	drop='default:dry_dirt',
-	sounds=default.node_sound_dirt_defaults(),
-	stack_max=40
+	drop = "default:dry_dirt",
+	sounds = default.node_sound_dirt_defaults(),
+	stack_max = 40
 })
 
--- Tree
 minetest.register_abm({
         nodenames = {"kalite:dirt"},
-        interval = 10,
-        chance = 1,
+        interval = 50,
+        chance = 4,
         action = function(pos, node)
-		local igniters = minetest.find_node_near(pos, 9, "group:igniter")
-		if igniters then
-			minetest.remove_node(igniters)
-		end
-		local ppos = {x = pos.x, y = pos.y+1, z = pos.z}
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
 		local na = minetest.get_node(ppos).name
 		if na ~= "default:tree" then
-			minetest.after(0.1, function ()
-				if math.random(1, 4) > 1 then
-					return
-				end
-		
-				local vm = minetest.get_voxel_manip()
-				local minp, maxp = vm:read_from_map({x=pos.x-16, y=pos.y+1, z=pos.z-16}, {x=pos.x+16, y=pos.y+16, z=pos.z+16})
-				local a = VoxelArea:new{MinEdge=minp, MaxEdge=maxp}
-				local data = vm:get_data()
-				default.grow_tree(data, a, ppos, math.random(1, 4) == 1, math.random(1,100000))
-				vm:set_data(data)
-				vm:write_to_map(data)
-				vm:update_map()
-				minetest.log("action", "A sapling grows into a tree at "..minetest.pos_to_string(pos))
-			end)
+			local vm = minetest.get_voxel_manip()
+			local minp, maxp = vm:read_from_map(
+				{x = pos.x - 16, y = pos.y + 1, z = pos.z - 16},
+				{x = pos.x + 16, y = pos.y + 16, z = pos.z + 16})
+			local a = VoxelArea:new{MinEdge = minp, MaxEdge = maxp}
+			local data = vm:get_data()
+			default.grow_tree(data, a, ppos, math.random(1, 4) == 1,
+					math.random(1, 100000))
+			vm:set_data(data)
+			vm:write_to_map(data)
+			vm:update_map()
+			minetest.log("action", "A sapling grows into a tree at " ..
+					minetest.pos_to_string(pos))
 		end
         end
 })
 
 
--- Wheat & Grass
-
+-- Wheat
 minetest.register_node("kalite:dirt2", {
-	description="Dirt (Wheat)",
-	tiles={"farming_soil.png", "default_dirt.png"},
-	is_ground_content=true,
-	groups={
-		crumbly=default.dig.dirt, soil=3
+	description = "Dirt (Wheat)",
+	tiles = {"farming_soil.png", "default_dirt.png"},
+	is_ground_content = true,
+	groups = {
+		crumbly = default.dig.dirt,
+		soil = 3,
+		not_in_creative_inventory = 1
 	},
-	drop='default:dry_dirt',
-	sounds=default.node_sound_dirt_defaults(),
-	stack_max=40
+	drop = "default:dry_dirt",
+	sounds = default.node_sound_dirt_defaults(),
+	stack_max = 40
 })
 
+minetest.register_abm({
+	nodenames = {"kalite:dirt2"},
+	interval = 25,
+	chance = 2,
+	action = function(pos, node)
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
+		local na = minetest.get_node(ppos).name
+		if not string.match(na, "farming:wheat_") then
+			return minetest.set_node(ppos,
+				{name = "farming:wheat_1"})
+		end
+	end
+})
+
+
+-- Cotton
 minetest.register_node("kalite:dirt3", {
-	description="Dirt (Cotton)",
-	tiles={"farming_soil.png", "default_dirt.png"},
+	description = "Dirt (Cotton)",
+	tiles = {"farming_soil.png", "default_dirt.png"},
 	is_ground_content=true,
-	groups={
-		crumbly=default.dig.dirt, soil=3
+	groups = {
+		crumbly = default.dig.dirt,
+		soil = 3,
+		not_in_creative_inventory = 1
 	},
-	drop='default:dry_dirt',
-	sounds=default.node_sound_dirt_defaults(),
-	stack_max=40
-})
-
-
-minetest.register_abm({
-	nodenames={"kalite:dirt2"},
-	interval = 10,
-	chance = 50,
-	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
-		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="farming:wheat_1"})
-	end
+	drop = "default:dry_dirt",
+	sounds = default.node_sound_dirt_defaults(),
+	stack_max = 40
 })
 
 minetest.register_abm({
-	nodenames={"kalite:dirt3"},
-	interval = 10,
-	chance = 50,
+	nodenames = {"kalite:dirt3"},
+	interval = 25,
+	chance = 2,
 	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
 		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="farming:cotton_1"})
+		if not string.match(na, "farming:cotton_") then
+			return minetest.set_node(ppos,
+				{name = "farming:cotton_1"})
+		end
 	end
 })
 
 
--- Papyrus & Cactus
-
+-- Papyrus
 minetest.register_node("kalite:sand1", {
 	description = "Sand (Papyrus)",
 	tiles = {"default_sand.png"},
 	is_ground_content = true,
 	groups = {
-		crumbly=default.dig.sand,
-		falling_node=0, -- Admin block must not move
-		sand=1,
-		water=3
+		crumbly = default.dig.sand,
+		falling_node = 0, -- Admin block must not move
+		sand = 1,
+		water = 3,
+		not_in_creative_inventory = 1
 	},
-	drop = 'default:sand',
+	drop = "default:sand",
 	sounds = default.node_sound_sand_defaults(),
-	stack_max = 40,
+	stack_max = 40
 })
 
+minetest.register_abm({
+	nodenames = {"kalite:sand1"},
+	interval = 25,
+	chance = 2,
+	action = function(pos, node)
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
+		local na = minetest.get_node(ppos).name
+		if na ~= "default:papyrus" then
+			return minetest.set_node(ppos,
+				{name = "default:papyrus"})
+		end
+	end
+})
+
+
+-- Cactus
 minetest.register_node("kalite:sand2", {
 	description = "Sand (Cactus)",
 	tiles = {"default_sand.png"},
 	is_ground_content = true,
 	groups = {
-		crumbly=default.dig.sand,
-		falling_node=0,
-		sand=1
+		crumbly = default.dig.sand,
+		falling_node = 0, -- Admin block must not move
+		sand = 1,
+		not_in_creative_inventory = 1
 	},
-	drop = 'default:sand',
+	drop = "default:sand",
 	sounds = default.node_sound_sand_defaults(),
-	stack_max = 40,
-})
-
-minetest.register_abm({
-	nodenames = {"kalite:sand1"},
-	interval = 10,
-	chance = 50,
-	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
-		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="default:papyrus"})
-	end
+	stack_max = 40
 })
 
 minetest.register_abm({
 	nodenames = {"kalite:sand2"},
-	interval = 10,
-	chance = 50,
+	interval = 25,
+	chance = 2,
 	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
 		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="default:cactus"})
+		if na ~= "default:cactus" then
+			return minetest.set_node(ppos,
+				{name = "default:cactus"})
+		end
 	end
 })
 
 
 -- Flowers
 
+-- Rose
 minetest.register_node("kalite:grass1", {
-        description = "Dirt with Grass (Red)",
-        tiles = {"default_grass.png", "default_dirt.png", "default_dirt.png^default_grass_side.png"},
+        description = "Dirt with Grass (Rose)",
+        tiles = {
+		"default_grass.png",
+		"default_dirt.png",
+		"default_dirt.png^default_grass_side.png"
+	},
         is_ground_content = true,
-        groups = {crumbly=default.dig.dirt_with_grass,soil=1,not_in_creative_inventory=1},
-        drop = 'default:dry_dirt',
+        groups = {
+		crumbly = default.dig.dirt_with_grass,
+		soil = 1,
+		not_in_creative_inventory = 1
+	},
+        drop = "default:dry_dirt",
         sounds = default.node_sound_dirt_defaults({
-                footstep = {name="default_grass_footstep", gain=0.25},
-        stack_max = 40,
-        }),
+                footstep = {
+			name = "default_grass_footstep",
+			gain = 0.25
+		}
+	}),
+        stack_max = 40
 })
 
 minetest.register_abm({
 	nodenames = {"kalite:grass1"},
-	interval = 20,
-	chance = 50,
+	interval = 25,
+	chance = 2,
 	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
 		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="flowers:flower_rose"})
+		if na ~= "flowers:flower_rose" then
+			return minetest.set_node(ppos,
+				{name = "flowers:flower_rose"})
+		end
 	end
 })
 
+-- Viola
 minetest.register_node("kalite:grass2", {
-        description = "Dirt with Grass (Purple?)",
-        tiles = {"default_grass.png", "default_dirt.png", "default_dirt.png^default_grass_side.png"},
+        description = "Dirt with Grass (Viola)",
+        tiles = {
+		"default_grass.png",
+		"default_dirt.png",
+		"default_dirt.png^default_grass_side.png"
+	},
         is_ground_content = true,
-        groups = {crumbly=default.dig.dirt_with_grass,soil=1,not_in_creative_inventory=1},
-        drop = 'default:dry_dirt',
+        groups = {
+		crumbly = default.dig.dirt_with_grass,
+		soil = 1,
+		not_in_creative_inventory = 1
+	},
+        drop = "default:dry_dirt",
         sounds = default.node_sound_dirt_defaults({
-                footstep = {name="default_grass_footstep", gain=0.25},
-        stack_max = 40,
+                footstep = {
+			name = "default_grass_footstep",
+			gain = 0.25
+		}
         }),
+        stack_max = 40
 })
 
 minetest.register_abm({
 	nodenames = {"kalite:grass2"},
-	interval = 20,
-	chance = 50,
+	interval = 25,
+	chance = 2,
 	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
 		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="flowers:flower_viola"})
+		if na ~= "flowers:flower_viola" then
+			return minetest.set_node(ppos,
+				{name = "flowers:flower_viola"})
+		end
 	end
 })
 
+-- Tulip
 minetest.register_node("kalite:grass3", {
-        description = "Dirt with Grass (Orange?)",
-        tiles = {"default_grass.png", "default_dirt.png", "default_dirt.png^default_grass_side.png"},
+        description = "Dirt with Grass (Tulip)",
+        tiles = {
+		"default_grass.png",
+		"default_dirt.png",
+		"default_dirt.png^default_grass_side.png"
+	},
         is_ground_content = true,
-        groups = {crumbly=default.dig.dirt_with_grass,soil=1,not_in_creative_inventory=1},
-        drop = 'default:dry_dirt',
+        groups = {
+		crumbly = default.dig.dirt_with_grass,
+		soil = 1,
+		not_in_creative_inventory = 1
+	},
+        drop = "default:dry_dirt",
         sounds = default.node_sound_dirt_defaults({
-                footstep = {name="default_grass_footstep", gain=0.25},
-        stack_max = 40,
+                footstep = {
+			name = "default_grass_footstep",
+			gain = 0.25
+		}
         }),
+        stack_max = 40
 })
 
 minetest.register_abm({
 	nodenames = {"kalite:grass3"},
-	interval = 20,
-	chance = 50,
+	interval = 25,
+	chance = 2,
 	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
 		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="flowers:flower_tulip"})
+		if na ~= "flowers:flower_tulip" then
+			return minetest.set_node(ppos,
+				{name = "flowers:flower_tulip"})
+		end
 	end
 })
 
+-- Geranium
 minetest.register_node("kalite:grass4", {
-        description = "Dirt with Grass (Blue?)",
-        tiles = {"default_grass.png", "default_dirt.png", "default_dirt.png^default_grass_side.png"},
+        description = "Dirt with Grass (Geranium)",
+        tiles = {
+		"default_grass.png",
+		"default_dirt.png",
+		"default_dirt.png^default_grass_side.png"
+	},
         is_ground_content = true,
-        groups = {crumbly=default.dig.dirt_with_grass,soil=1,not_in_creative_inventory=1},
-        drop = 'default:dry_dirt',
+        groups = {
+		crumbly = default.dig.dirt_with_grass,
+		soil = 1,
+		not_in_creative_inventory = 1
+	},
+        drop = "default:dry_dirt",
         sounds = default.node_sound_dirt_defaults({
-                footstep = {name="default_grass_footstep", gain=0.25},
-        stack_max = 40,
+                footstep = {
+			name = "default_grass_footstep",
+			gain = 0.25
+		}
         }),
+        stack_max = 40
 })
 
 minetest.register_abm({
 	nodenames = {"kalite:grass4"},
-	interval = 20,
-	chance = 50,
+	interval = 25,
+	chance = 2,
 	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
 		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="flowers:flower_geranium"})
+		if na ~= "flowers:flower_geranium" then
+			return minetest.set_node(ppos,
+				{name = "flowers:flower_geranium"})
+		end
 	end
 })
+
+-- Dandelion (Yellow)
 minetest.register_node("kalite:grass5", {
-        description = "Dirt with Grass (Yellow)",
-        tiles = {"default_grass.png", "default_dirt.png", "default_dirt.png^default_grass_side.png"},
+        description = "Dirt with Grass (Yellow Dandelion)",
+        tiles = {
+		"default_grass.png",
+		"default_dirt.png",
+		"default_dirt.png^default_grass_side.png"
+	},
         is_ground_content = true,
-        groups = {crumbly=default.dig.dirt_with_grass,soil=1,not_in_creative_inventory=1},
-        drop = 'default:dry_dirt',
+        groups = {
+		crumbly = default.dig.dirt_with_grass,
+		soil = 1,
+		not_in_creative_inventory = 1
+	},
+        drop = "default:dry_dirt",
         sounds = default.node_sound_dirt_defaults({
-                footstep = {name="default_grass_footstep", gain=0.25},
-        stack_max = 40,
+                footstep = {
+			name = "default_grass_footstep",
+			gain = 0.25
+		}
         }),
+        stack_max = 40
 })
 
 minetest.register_abm({
 	nodenames = {"kalite:grass5"},
-	interval = 20,
-	chance = 50,
+	interval = 25,
+	chance = 2,
 	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
 		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="flowers:flower_dandelion_yellow"})
+		if na ~= "flowers:flower_dandelion_yellow" then
+			return minetest.set_node(ppos,
+				{name = "flowers:flower_dandelion_yellow"})
+		end
 	end
 })
+
+-- Dandeolion (White)
 minetest.register_node("kalite:grass6", {
-        description = "Dirt with Grass (White)",
-        tiles = {"default_grass.png", "default_dirt.png", "default_dirt.png^default_grass_side.png"},
+        description = "Dirt with Grass (White Dandelion)",
+        tiles = {
+		"default_grass.png",
+		"default_dirt.png",
+		"default_dirt.png^default_grass_side.png"
+	},
         is_ground_content = true,
-        groups = {crumbly=default.dig.dirt_with_grass,soil=1,not_in_creative_inventory=1},
-        drop = 'default:dry_dirt',
+        groups = {
+		crumbly = default.dig.dirt_with_grass,
+		soil = 1,
+		not_in_creative_inventory = 1
+	},
+        drop = "default:dry_dirt",
         sounds = default.node_sound_dirt_defaults({
-                footstep = {name="default_grass_footstep", gain=0.25},
-        stack_max = 40,
+                footstep = {
+			name = "default_grass_footstep",
+			gain = 0.25
+		}
         }),
+        stack_max = 40
 })
 
 minetest.register_abm({
 	nodenames = {"kalite:grass6"},
-	interval = 20,
-	chance = 50,
+	interval = 25,
+	chance = 2,
 	action = function(pos, node)
-		local ppos = {x=pos.x, y=pos.y+1, z=pos.z}
+		remove_igniters(pos)
+		local ppos = {x = pos.x, y = pos.y + 1, z = pos.z}
 		local na = minetest.get_node(ppos).name
-		if na ~= "air" then return end
-		minetest.set_node(ppos, {name="flowers:flower_dandelion_white"})
+		if na ~= "flowers:flower_dandelion_white" then
+			return minetest.set_node(ppos,
+				{name = "flowers:flower_dandelion_white"})
+		end
 	end
 })
