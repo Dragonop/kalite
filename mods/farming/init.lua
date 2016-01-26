9e7de28644c8787f66ea87cb4ac40a4e7ebaf7a3
James Stevenson <everamzah@gmail.com>
2016-01-26 03:57:20 -0500

    Touch farming

--- mods/farming/init.lua
+++ mods/farming/init.lua
@@ -215,7 +215,6 @@ end
 minetest.register_tool("farming:hoe_wood", {
 	description = "Wooden Hoe",
 	inventory_image = "farming_tool_woodhoe.png",
-	
 	on_use = function(itemstack, user, pointed_thing)
 		return hoe_on_use(itemstack, user, pointed_thing, 60/3)
 	end,
@@ -224,7 +223,6 @@ minetest.register_tool("farming:hoe_wood", {
 minetest.register_tool("farming:hoe_stone", {
 	description = "Stone Hoe",
 	inventory_image = "farming_tool_stonehoe.png",
-	
 	on_use = function(itemstack, user, pointed_thing)
 		return hoe_on_use(itemstack, user, pointed_thing, 132/3)
 	end,
@@ -239,20 +237,10 @@ minetest.register_tool("farming:hoe_iron", {
 	end,
 })
 
-minetest.register_tool("farming:hoe_gold", {
-	description = "Gold Hoe",
-	inventory_image = "farming_tool_goldhoe.png",
-	
-	on_use = function(itemstack, user, pointed_thing)
-		return hoe_on_use(itemstack, user, pointed_thing, 33/3)
-	end,
-})
-
 minetest.register_alias("farming:hoe_diamond", "farming:hoe_bronze")
 minetest.register_tool("farming:hoe_bronze", {
 	description = "Bronze Hoe",
 	inventory_image = "farming_tool_bronzehoe.png",
-	
 	on_use = function(itemstack, user, pointed_thing)
 		return hoe_on_use(itemstack, user, pointed_thing, 1562/3)
 	end,
@@ -286,15 +274,6 @@ minetest.register_craft({
 })
 
 minetest.register_craft({
-	output = "farming:hoe_gold",
-	recipe = {
-		{"default:gold_ingot", "default:gold_ingot"},
-		{"", "group:stick"},
-		{"", "group:stick"},
-	}
-})
-
-minetest.register_craft({
 	output = "farming:hoe_bronze",
 	recipe = {
 		{"default:bronze_ingot", "default:bronze_ingot"},
@@ -302,9 +281,11 @@ minetest.register_craft({
 		{"", "group:stick"},
 	}
 })
+
 --
 -- Place seeds
 --
+
 local function place_seed(itemstack, placer, pointed_thing, plantname)
 	local pt = pointed_thing
 	-- check if pointing at a node
@@ -352,38 +333,42 @@ end
 --
 -- Wheat
 --
+
 minetest.register_craftitem("farming:seed_wheat", {
 	description = "Wheat Seed",
 	inventory_image = "farming_wheat_seed.png",
 	on_place = function(itemstack, placer, pointed_thing)
 		return place_seed(itemstack, placer, pointed_thing, "farming:wheat_1")
 	end,
-	stack_max = 60,
+	stack_max = 60
 })
 
 minetest.register_craftitem("farming:wheat", {
 	description = "Wheat",
 	inventory_image = "farming_wheat.png",
-	stack_max = 60,
+	stack_max = 60
 })
 
 minetest.register_craftitem("farming:flour", {
 	description = "Flour",
 	inventory_image = "farming_flour.png",
-	stack_max = 60,
+	stack_max = 60
 })
 
 minetest.register_craftitem("farming:bread", {
 	description = "Bread",
 	inventory_image = "farming_bread.png",
 	on_use = minetest.item_eat(4),
-	stack_max = 60,
+	stack_max = 60
 })
 
 minetest.register_craft({
 	type = "shapeless",
 	output = "farming:flour",
-	recipe = {"farming:wheat", "farming:wheat", "farming:wheat", "farming:wheat"}
+	recipe = {
+		"farming:wheat", "farming:wheat",
+		"farming:wheat", "farming:wheat"
+	}
 })
 
 minetest.register_craft({
@@ -393,13 +378,13 @@ minetest.register_craft({
 	recipe = "farming:flour"
 })
 
-for i=1,8 do
+for i = 1, 8 do
 	local drop = {
 		items = {
-			{items = {'farming:wheat'},rarity=9-i},
-			{items = {'farming:wheat'},rarity=18-i*2},
-			{items = {'farming:seed_wheat'},rarity=9-i},
-			{items = {'farming:seed_wheat'},rarity=18-i*2},
+			{items = {"farming:wheat"}, rarity = 9 - i},
+			{items = {"farming:wheat"}, rarity = 18 - i * 2},
+			{items = {"farming:seed_wheat"}, rarity = 9 - i},
+			{items = {"farming:seed_wheat"}, rarity = 18 - i * 2}
 		}
 	}
 	minetest.register_node("farming:wheat_"..i, {
@@ -414,8 +399,15 @@ for i=1,8 do
 			type = "fixed",
 			fixed = {-0.5, -0.5, -0.5, 0.5, -5/16, 0.5},
 		},
-		groups = {dig=default.dig.instant,flammable=2,plant=1,wheat=i,not_in_creative_inventory=1,attached_node=1},
-		sounds = default.node_sound_leaves_defaults(),
+		groups = {
+			dig = default.dig.instant,
+			flammable = 2,
+			plant = 1,
+			wheat = i,
+			not_in_creative_inventory = 1,
+			attached_node = 1
+		},
+		sounds = default.node_sound_leaves_defaults()
 	})
 end
 
@@ -455,19 +447,20 @@ minetest.register_abm({
 --
 -- Cotton
 --
+
 minetest.register_craftitem("farming:seed_cotton", {
 	description = "Cotton Seed",
 	inventory_image = "farming_cotton_seed.png",
 	on_place = function(itemstack, placer, pointed_thing)
 		return place_seed(itemstack, placer, pointed_thing, "farming:cotton_1")
 	end,
-	stack_max = 60,
+	stack_max = 60
 })
 
 minetest.register_craftitem("farming:string", {
 	description = "String",
 	inventory_image = "farming_string.png",
-	stack_max = 60,
+	stack_max = 60
 })
 
 minetest.register_craft({
