8cafbe5bb401454c27d20084aedf5e6a3a97d843
James Stevenson <everamzah@gmail.com>
2016-02-01 06:34:37 -0500

    Dry grass gets dirt_with_grass rating

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -219,7 +219,7 @@ minetest.register_node("default:dry_grass", {
 		"default_dirt.png",
 		{name = "default_dirt.png^default_dry_grass_side.png",
 			tileable_vertical = false}},
-	groups = {crumbly = default.dig.dirt, soil = 1},
+	groups = {crumbly = default.dig.dirt_with_grass, soil = 1},
 	drop = "default:dry_dirt",
 	sounds = default.node_sound_dirt_defaults({
 		footstep = {name = "default_grass_footstep", gain = 0.4},
