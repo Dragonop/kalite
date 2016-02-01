a38df2cbc2b8a9845e72621921b81d69daef5c28
James Stevenson <everamzah@gmail.com>
2016-02-01 14:43:13 -0500

    Chess in Kalite Start with an empty xdecor directory (sans realchess) TODO move rope and craftguide to this directory

---
+++ mods/xdecor/init.lua
@@ -0,0 +1,21 @@
+--[[local t = os.clock()
+xdecor = {}--]]
+local modpath = minetest.get_modpath("xdecor")--[[
+
+dofile(modpath.."/handlers/nodeboxes.lua")
+dofile(modpath.."/handlers/registration.lua")--]]
+dofile(modpath.."/chess.lua")
+--[[dofile(modpath.."/cooking.lua")
+dofile(modpath.."/craftitems.lua")
+dofile(modpath.."/enchanting.lua")
+dofile(modpath.."/hive.lua")
+dofile(modpath.."/itemframe.lua")
+dofile(modpath.."/mailbox.lua")
+dofile(modpath.."/nodes.lua")
+dofile(modpath.."/recipes.lua")
+dofile(modpath.."/rope.lua")
+dofile(modpath.."/sitting.lua")
+dofile(modpath.."/worktable.lua")
+dofile(modpath.."/xwall.lua")
+--print(string.format("xdecor loaded in %.2f ms", (os.clock()-t)*1000))--]]
+
